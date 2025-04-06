3195eb75b42cf47eb7308fc41d8dafeae77f00f9
dw642 <dw642@cornell.edu>
2025-04-06 15:14:40 -0400

    first commit

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const TOKEN_KEY = "token";
+//export const BASE_URL = "https://halogen-ethos-449617-g7.uw.r.appspot.com";
+export const BASE_URL = "https://healthy-mender-449617-t1.uk.r.appspot.com";
+export const SEARCH_KEY = {
+  all: 0,
+  keyword: 1,
+  user: 2,
+};
